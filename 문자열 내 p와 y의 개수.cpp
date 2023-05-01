4eb1fa1f2e7bf1ca152f137ad872851c20b383d0
Shihong Park <78722713+Fresh-hongsi@users.noreply.github.com>
2023-05-01 14:22:40 +0900

    Create 문자열 내 p와 y의 개수.cpp

---
+++ 문자열 내 p와 y의 개수.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    
+    int pCount=0;
+    int yCount=0;
+    
+    for(int i=0;i<s.length();i++)
+    {
+        if(s[i]=='p' || s[i]=='P')
+        {
+            pCount++;
+        }
+        
+        if(s[i]=='y' || s[i]=='Y')
+        {
+            yCount++;
+        }
+    }
+    
+    if(pCount!=yCount)
+    {
+        answer=false;
+    }
+    
+
+    return answer;
+}

ce52e7de517114590e8df3a5fe5032e258130f8e
Shihong Park <78722713+Fresh-hongsi@users.noreply.github.com>
2024-04-11 19:16:23 +0900

    Create 타겟 넘버.cpp

---
+++ Lv.2/타겟 넘버.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int cnt = 0;
+vector<int> arr;
+
+void dfs(int cur, int length, int goal){
+    
+    if(length==arr.size()){
+        
+        if(cur==goal){
+            cnt++;
+        }
+        return;
+    }
+    
+
+    dfs(cur+arr[length],length+1, goal);
+    dfs(cur-arr[length],length+1, goal);
+    
+}
+
+int solution(vector<int> numbers, int target) {
+    int answer = 0;
+    
+    
+    for(int i=0;i<numbers.size();i++){
+        arr.push_back(numbers[i]);
+    }
+    
+    dfs(arr[0],1,target);
+    dfs(-arr[0],1,target);
+    answer = cnt;
+    return answer;
+}

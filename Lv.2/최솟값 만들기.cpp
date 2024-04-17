7a640493b313946d42678c29c341621479b61d4f
Shihong Park <78722713+Fresh-hongsi@users.noreply.github.com>
2024-04-17 10:47:41 +0900

    Update 최솟값 만들기.cpp

--- Lv.2/최솟값 만들기.cpp
+++ Lv.2/최솟값 만들기.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
-#include <algorithm>
 #include<vector>
+#include <algorithm>
+#include <functional>
 using namespace std;
 
 int solution(vector<int> A, vector<int> B)
 {
     int answer = 0;
-    // 배열 A는 오름차순 , 배멸 B는 내림차순으로 했을 때, 누적된 값의 합이 최소가 된다
-    sort(A.begin(),A.end()); // 오름차순 정렬
-    sort(B.begin(),B.end(),greater<>()); // 내림차순 정렬
-    
-    for(int i=0;i<A.size();i++){ // 누적된 값의 최소값 계산
-        answer+=A[i]*B[i];
-    }
-    
 
+    sort(A.begin(),A.end());
+    sort(B.begin(),B.end(),greater<int>());
+    
+    for(int i=0;i<A.size();i++){
+        int temp = 0;
+        temp=A[i]*B[i];
+        answer+=temp;
+    }
     return answer;
 }

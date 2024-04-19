15a9e2f2c11214c89e617da6d18dee3b752afc04
Shihong Park <78722713+Fresh-hongsi@users.noreply.github.com>
2024-04-19 13:53:57 +0900

    Update 이진 변환 반복하기.cpp

--- Lv.2/이진 변환 반복하기.cpp
+++ Lv.2/이진 변환 반복하기.cpp
@@ -1,69 +1,57 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <functional>
+#include <iostream>
 using namespace std;
 
+int zCount = 0;
+int convNum = 0;
+
+string eraseZero(string s){
+    string temp = "";
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='0'){
+            zCount++;
+        }
+        else{
+            temp+=s[i];
+        }
+    }
+    return temp;
+}
+
+string convTwo(int c){
+    string temp = "";
+    convNum++;
+    while(c!=0){
+        int t = c%2;
+        c/=2;
+        temp+=to_string(t);
+    }
+    
+    reverse(temp.begin(),temp.end());
+    return temp;
+    
+}
 vector<int> solution(string s) {
     vector<int> answer;
-    
-    int zeroErasedNum=0; // 제거된 0의 개수
-    int conversionNum=1; // 진번 변환 횟수
-    string stringSetter=s; // 진법 변환을 할때마다 다시 새로운 문자열로 세팅됨
-    string tempString=""; // 각 진법 변환시마다 0을 지운 결과가 저장되는 문자열
-    
-    while(1)
-    {
-        // 0을 제거하여 tempString에 저장
-        for(int i=0;i<stringSetter.length();i++)
-        {
-            if(stringSetter[i]=='0')
-            {
-                zeroErasedNum++;
-            }
-            else
-            {
-                tempString+="1";
-            }
-        }
-    
+
+    while(s!="1"){
+        // 0을 제거한 문자열 반환
+        s = eraseZero(s);
         
-        int length = tempString.length();
-        string conversionToBinaryReverse=""; // 거꾸로 된 이진법 변환 문자열
+        // 0이 제거된 문자열의 길이에 대응하는 수를 이진수로 변환한 문자열 구하기
+        int cur = s.length();
+        string conv = convTwo(cur);
+        // 그 문자열을 s로 다시 초기화 
         
-        // 거꾸로 된 이진변환 문자열 구하기
-        while(length!=0)
-        {
-            if(length%2==0)
-            {
-                conversionToBinaryReverse+="0";
-                length/=2;
-            }
-            else{
-                conversionToBinaryReverse+="1";
-                length/=2;
-            }
-        
-        }
-        
-        // 다시 거꾸로 해야 정상적인 2진법 변환
-        reverse(conversionToBinaryReverse.begin(), conversionToBinaryReverse.end()); 
-    
-        // 만약 1이 아니면 다시 시도
-        if(conversionToBinaryReverse!="1")
-        {
-            stringSetter=conversionToBinaryReverse;
-            tempString="";
-            conversionNum++;
-            continue;
-        }
-        else
-        {
-            break;
-        }
+        s = conv;
     }
     
     
-    answer.push_back(conversionNum);
-    answer.push_back(zeroErasedNum);
+    answer.push_back(convNum);
+    answer.push_back(zCount);
     return answer;
 }
+

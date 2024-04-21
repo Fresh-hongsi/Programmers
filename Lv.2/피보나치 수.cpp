e1c4db72a15418fb946438cb4a2711e40158bcc2
Shihong Park <78722713+Fresh-hongsi@users.noreply.github.com>
2024-04-21 23:32:24 +0900

    Update 피보나치 수.cpp

--- Lv.2/피보나치 수.cpp
+++ Lv.2/피보나치 수.cpp
@@ -1,20 +1,20 @@
 #include <string>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
 
 int solution(int n) {
-    int answer = 0;
-    vector<int> arr(100001,0);
     
-    arr[0]=0;
-    arr[1]=1;
+    vector<int> fibo(100001,0);
+    fibo[0]=0;
+    fibo[1]=1;
+    fibo[2]=1;
     
-    for(int i=2;i<=n;i++)
-    {
-        arr[i]=(((arr[i-1]%1234567)+(arr[i-2]%1234567))%1234567);
+    for(int i=3;i<=n;i++){
+        fibo[i]=(fibo[i-2]+fibo[i-1])%1234567;
     }
     
-    answer= arr[n];
+    int answer = fibo[n];
     return answer;
+    
 }

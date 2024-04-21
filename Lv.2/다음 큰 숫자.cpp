dd6c86c753a242386ebb642d5f87374f47aee03e
Shihong Park <78722713+Fresh-hongsi@users.noreply.github.com>
2024-04-21 13:37:08 +0900

    Update 다음 큰 숫자.cpp

--- Lv.2/다음 큰 숫자.cpp
+++ Lv.2/다음 큰 숫자.cpp
@@ -1,63 +1,99 @@
+// #include <string>
+// #include <vector>
+
+// using namespace std;
+
+// int solution(int n) {
+//     int answer = 0;
+    
+//     string cur="";
+//     string next="";
+//     int cur1Count=0; // 처음 들어온 n값에 대한 1의 개수
+//     int next1Count=0; // 앞으로 계속 n값을 증가시켜보면서 1값을 count할 변수
+//     int temp=n; // 처음엔 temp변수에 n을 세팅
+    
+//     // 처음 수 n에 대해서 1의 갯수 count하기
+//     while(temp>0)
+//     {
+//         if(temp%2==0)
+//         {
+//             cur+='0';
+//         }
+//         else
+//         {
+//             cur+='1';
+//             cur1Count++;
+//         }
+//         temp/=2;
+//     }
+    
+//     // n값을 1씩 증가시켜보면서 해당 수에 1이 몇개 있는지 count하기
+//     while(1)
+//     {
+//         n+=1;
+//         temp=n;
+//         next1Count=0;
+        
+//         while(temp>0)
+//         {
+//             if(temp%2==0)
+//             {
+//                 next+='0';
+//             }
+//             else
+//             {
+//                 next+='1';
+//                 next1Count++;
+//             }
+//             temp/=2;
+//         }
+        
+//         // 처음 들어온 수의 1개수와 1씩 증가시켜보면서 count한 1의 갯수가 같다면 while문 종료
+//         if(cur1Count==next1Count)
+//         {
+//             answer=n;
+//             break;
+//         }
+        
+        
+//     }
+    
+    
+//     return answer;
+// }
+
 #include <string>
 #include <vector>
-
+#include <algorithm>
+#include <functional>
 using namespace std;
 
-int solution(int n) {
-    int answer = 0;
+int check(int k){ // 2진수로 변환 후, 1의 개수가 몇개인지 반환하는 함수
     
-    string cur="";
-    string next="";
-    int cur1Count=0; // 처음 들어온 n값에 대한 1의 개수
-    int next1Count=0; // 앞으로 계속 n값을 증가시켜보면서 1값을 count할 변수
-    int temp=n; // 처음엔 temp변수에 n을 세팅
+    int numOfOne = 0;
     
-    // 처음 수 n에 대해서 1의 갯수 count하기
-    while(temp>0)
-    {
-        if(temp%2==0)
-        {
-            cur+='0';
+    while(k!=0){
+        if((k%2)==1){
+            numOfOne++;
         }
-        else
-        {
-            cur+='1';
-            cur1Count++;
-        }
-        temp/=2;
+        k/=2;
     }
     
-    // n값을 1씩 증가시켜보면서 해당 수에 1이 몇개 있는지 count하기
-    while(1)
-    {
-        n+=1;
-        temp=n;
-        next1Count=0;
+    return numOfOne;
+}
+
+int solution(int n) {
+    int answer = n;
+    
+    int numOfOneInN = check(n);
+    
+    while(1){
+        answer++;
         
-        while(temp>0)
-        {
-            if(temp%2==0)
-            {
-                next+='0';
-            }
-            else
-            {
-                next+='1';
-                next1Count++;
-            }
-            temp/=2;
-        }
-        
-        // 처음 들어온 수의 1개수와 1씩 증가시켜보면서 count한 1의 갯수가 같다면 while문 종료
-        if(cur1Count==next1Count)
-        {
-            answer=n;
+        int numOfOneInAnswer = check(answer);
+        if(numOfOneInN==numOfOneInAnswer){
             break;
         }
-        
-        
     }
-    
-    
     return answer;
 }

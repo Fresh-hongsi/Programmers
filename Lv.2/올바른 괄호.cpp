afd63663bc50d824f58a8c442997aaa7d8f8e72f
Shihong Park <78722713+Fresh-hongsi@users.noreply.github.com>
2024-06-07 15:16:37 +0900

    Update 올바른 괄호.cpp

--- Lv.2/올바른 괄호.cpp
+++ Lv.2/올바른 괄호.cpp
@@ -29,3 +29,52 @@ bool solution(string s)
     }
     return answer;
 }
+
+
+// #include <string>
+// #include <iostream>
+// #include <algorithm>
+// #include <vector>
+// #include <stack>
+// #include <functional>
+
+// using namespace std;
+
+// bool solution(string s)
+// {
+//     bool answer = true;
+
+//     stack<char> st;
+    
+//     for(int i=0;i<s.length();i++){
+//         char cur = s[i];
+        
+//         if(cur=='('){
+//             st.push(cur);
+//         }
+//         else{
+//             if(!st.empty()){
+//               char top = st.top();
+//                 if(top=='('){
+//                     st.pop();
+//                 }
+//                 else{
+//                     st.push(')');
+//                 }  
+//             }
+//             else{
+//                 st.push(')');
+//             }
+            
+//         }
+//     }
+    
+//     if(!st.empty()){
+//         answer=false;
+//     }
+//     else{
+//         answer=true;
+//     }
+
+//     return answer;
+// }

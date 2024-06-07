fa47ae7d3b9802b98a5f62b83b5a0703aac5e9f3
Shihong Park <78722713+Fresh-hongsi@users.noreply.github.com>
2024-06-07 15:08:38 +0900

    Update 최댓값과 최솟값.cpp

--- Lv.2/최댓값과 최솟값.cpp
+++ Lv.2/최댓값과 최솟값.cpp
@@ -3,32 +3,33 @@
 #include <algorithm>
 #include <functional>
 #include <iostream>
-
 using namespace std;
 
 string solution(string s) {
+    string answer = "";
     
     vector<int> arr;
     string temp = "";
     
     for(int i=0;i<s.length();i++){
-        if(s[i]==' '){
+        char cur = s[i];
+        if(cur==' '){
             arr.push_back(stoi(temp));
             temp="";
+            continue;
         }
-        
         else{
-            temp+=s[i];
+            temp+=(cur);
         }
     }
     arr.push_back(stoi(temp));
     
-    int minimum = *min_element(arr.begin(),arr.end());
-    int maximum = *max_element(arr.begin(),arr.end());
+    int min = *min_element(arr.begin(),arr.end());
+    int max = *max_element(arr.begin(),arr.end());
     
-    string answer = "";
-    answer+=to_string(minimum);
-    answer+=" ";
-    answer+=to_string(maximum);
+    answer+=to_string(min);
+    answer+=' ';
+    answer+=to_string(max);
+    
     return answer;
 }

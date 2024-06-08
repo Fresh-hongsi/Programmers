a535908fa592a5f5805024ff9560ae67e0524b7c
Shihong Park <78722713+Fresh-hongsi@users.noreply.github.com>
2024-06-08 14:05:37 +0900

    Update JadenCase 문자열 만들기.cpp

--- Lv.2/JadenCase 문자열 만들기.cpp
+++ Lv.2/JadenCase 문자열 만들기.cpp
@@ -17,3 +17,33 @@ string solution(string s) {
     
     return answer;
 }
+
+
+// #include <string>
+// #include <vector>
+// #include <iostream>
+// #include <algorithm>
+// #include <functional>
+
+// using namespace std;
+
+// string solution(string s) {
+//     string answer = "";
+    
+//     for(int i=0;i<s.length();i++){
+//         if(i==0){
+//             s[i]=toupper(s[i]);
+//         }
+        
+//         else if(i>=1 && s[i-1]==' '){
+//             s[i]=toupper(s[i]);
+//         }
+        
+//         else{
+//             s[i]=tolower(s[i]);
+//         }
+//     }
+//     answer=s;
+//     return answer;
+    
+// }

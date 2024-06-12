f3b2d5e427728f136026428d1502324acf06c182
Shihong Park <78722713+Fresh-hongsi@users.noreply.github.com>
2024-06-12 14:33:48 +0900

    Update 점프와 순간 이동.cpp

--- Lv.2/점프와 순간 이동.cpp
+++ Lv.2/점프와 순간 이동.cpp
@@ -1,22 +1,47 @@
+// #include <iostream>
+// #include <algorithm>
+// #include <vector>
+// using namespace std;
+
+// int solution(int n)
+// {
+//     int ans = 0;
+    
+//     while(n!=0){ // n이 0이 될 때까지 수를 작게 나누기
+//     {
+//         if(n%2==0){ // 나머지가 0이면 순간이동 전 값이 최소값임
+//             n/=2;
+//         }
+//         else{ // 나머지가 1이면 순간이동 전 값 + 1 해준 게 최소값임
+//             ans++;
+//             n-=1;
+//         }
+//     }
+   
+//     return ans;
+// }
+
+
 #include <iostream>
 #include <algorithm>
+#include <string>
 #include <vector>
+#include <functional>
 using namespace std;
 
 int solution(int n)
 {
     int ans = 0;
-    
-    while(n!=0){ // n이 0이 될 때까지 수를 작게 나누기
-    {
-        if(n%2==0){ // 나머지가 0이면 순간이동 전 값이 최소값임
+    while(n>0){
+        if(n%2==0){
             n/=2;
         }
-        else{ // 나머지가 1이면 순간이동 전 값 + 1 해준 게 최소값임
+        else{
+            n/=2;
             ans++;
-            n-=1;
         }
     }
-   
     return ans;
 }
+
+

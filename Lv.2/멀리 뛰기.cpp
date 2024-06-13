ccc36423a9e4d1ab77f3ff765af4c333054668c2
Shihong Park <78722713+Fresh-hongsi@users.noreply.github.com>
2024-06-13 18:50:39 +0900

    Update 멀리 뛰기.cpp

--- Lv.2/멀리 뛰기.cpp
+++ Lv.2/멀리 뛰기.cpp
@@ -21,24 +21,44 @@
 //     answer = dp[n];
 //     return answer;
 // }
+// #include <string>
+// #include <vector>
+// #include <algorithm>
+// using namespace std;
+
+// long long solution(int n) {
+//     long long answer = 0;
+    
+//     vector<int> dp(2001,0);
+//     dp[0]=0;
+//     dp[1]=1;
+//     dp[2]=2;
+//     dp[3]=3;
+//     dp[4]=5;
+    
+//     for(int i=5;i<=n;i++){
+//         dp[i]=(dp[i-1]+dp[i-2])%1234567;
+//     }
+//     answer = dp[n];
+//     return answer;
+// }
+
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <iostream>
+#include <functional>
 using namespace std;
 
 long long solution(int n) {
     long long answer = 0;
-    
-    vector<int> dp(2001,0);
-    dp[0]=0;
+    long long dp[2001]={0,};
+    dp[0]=1;
     dp[1]=1;
-    dp[2]=2;
-    dp[3]=3;
-    dp[4]=5;
     
-    for(int i=5;i<=n;i++){
-        dp[i]=(dp[i-1]+dp[i-2])%1234567;
+    for(int i=2;i<=n;i++){
+        dp[i]=(dp[i-2]+dp[i-1])%1234567;
     }
-    answer = dp[n];
+    answer=dp[n];
     return answer;
 }

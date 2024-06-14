05cd947f3bfe1fbda5675e533ef1be15022a2190
Shihong Park <78722713+Fresh-hongsi@users.noreply.github.com>
2024-06-14 09:05:17 +0900

    Update 영어 끝말잇기.cpp

--- Lv.2/영어 끝말잇기.cpp
+++ Lv.2/영어 끝말잇기.cpp
@@ -74,63 +74,127 @@
 //     // 그 외의 경우, 세팅된 값을 반환
 //     return answer;
 // }
+// #include <string>
+// #include <vector>
+// #include <iostream>
+// #include <algorithm>
+// #include <set>
+// using namespace std;
+
+// vector<int> solution(int n, vector<string> words) {
+    
+//     bool flag = true;
+//     vector<int> answer;
+//     set<string> s;
+//     s.insert(words[0]);
+//     int who = 0; // 틀린 사람 번호
+//     int num = 0; // 몇번째 턴에서 틀렸는지
+//     for(int i=1;i<words.size();i++){
+        
+//         string front = words[i-1];
+//         string cur = words[i];
+        
+//         // cur이 이미 나온 단어라면
+//         if(s.find(cur)!=s.end()){
+//             flag = false;
+//             who = (i+1)%n;
+//             if(who==0){
+//                 who=n;
+//             }
+//             num = i/n+1;
+//             break;
+//         }
+        
+//         // cur의 첫번째 문자와 front의 마지막 문자가 다르다면
+//         else if(front[front.length()-1]!=cur[0]){
+           
+//             flag = false;
+//             who = (i+1)%n;
+//             if(who==0){
+//                 who=n;
+//             }
+//             num = i/n+1;
+//             break;
+//         }
+        
+//         // 그 외의 경우 set에 현재 cur 넣기
+//         else{
+//             s.insert(cur);
+//         }
+        
+//     }
+    
+//     if(flag==true){
+//         answer.push_back(0);
+//         answer.push_back(0);
+//     }
+//     else{
+//         answer.push_back(who);
+//         answer.push_back(num);
+//     }
+//     return answer;
+// }
+
+
 #include <string>
 #include <vector>
 #include <iostream>
-#include <algorithm>
 #include <set>
+#include <functional>
+#include <algorithm>
 using namespace std;
 
 vector<int> solution(int n, vector<string> words) {
-    
-    bool flag = true;
     vector<int> answer;
-    set<string> s;
-    s.insert(words[0]);
-    int who = 0; // 틀린 사람 번호
-    int num = 0; // 몇번째 턴에서 틀렸는지
-    for(int i=1;i<words.size();i++){
+    set<string> stringSet;
+    
+    for(int i=1;i<=words.size();i++){
         
-        string front = words[i-1];
-        string cur = words[i];
-        
-        // cur이 이미 나온 단어라면
-        if(s.find(cur)!=s.end()){
-            flag = false;
-            who = (i+1)%n;
-            if(who==0){
-                who=n;
-            }
-            num = i/n+1;
-            break;
+        if(stringSet.size()==0){
+            stringSet.insert(words[i-1]);
         }
         
-        // cur의 첫번째 문자와 front의 마지막 문자가 다르다면
-        else if(front[front.length()-1]!=cur[0]){
-           
-            flag = false;
-            who = (i+1)%n;
-            if(who==0){
-                who=n;
-            }
-            num = i/n+1;
-            break;
-        }
-        
-        // 그 외의 경우 set에 현재 cur 넣기
         else{
-            s.insert(cur);
+            string cur = words[i-1];
+            string front = words[i-2];
+            
+            if((stringSet.find(cur)!=stringSet.end()) || (front[front.length()-1]!=cur[0]) ){ // 이미 있는 단어거나 끝말잇기가 불가능한 단어일 경우
+                
+                //몇번째 사람이 몇번째 시도에 걸린 건지 구하기
+                if(i%n==0){
+                    answer.push_back(n);    
+                }
+                else{
+                    answer.push_back(i%n);    
+                }
+                
+                if(i/n==0){
+                    answer.push_back(1);
+                }
+                else{
+                    if(i%n==0){
+                        answer.push_back((i/n));
+                    }
+                    else{
+                        answer.push_back((i/n)+1);
+                    }
+                }
+                
+                break;
+            }
+            
+            else{ // 끝말잇기가 가능한 정상적인 경우 -> 단어 추가
+                stringSet.insert(cur);
+            }
         }
-        
     }
     
-    if(flag==true){
+    // 아무도 안걸린 경우
+    if(answer.size()==0){
         answer.push_back(0);
         answer.push_back(0);
     }
-    else{
-        answer.push_back(who);
-        answer.push_back(num);
-    }
+    
+    
     return answer;
 }

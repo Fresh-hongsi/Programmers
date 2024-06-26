10741f95d60b5f73e19847bd261ca860b608bc79
Shihong Park <78722713+Fresh-hongsi@users.noreply.github.com>
2024-06-26 15:51:39 +0900

    Update n^2 배열 자르기.cpp

--- Lv.2/n^2 배열 자르기.cpp
+++ Lv.2/n^2 배열 자르기.cpp
@@ -1,29 +1,57 @@
+// #include <string>
+// #include <vector>
+// #include <algorithm>
+// #include <functional>
+// #include <iostream>
+// using namespace std;
+
+// vector<int> solution(int n, long long left, long long right) {
+    
+//     vector<int> answer;
+    
+//     for(long long i = left; i<=right;i++){
+//         long long quotient = i/n;
+//         long long remainder = i%n;
+        
+//         if(remainder>=quotient){
+//             answer.push_back(remainder+1);
+//         }
+//         else{
+//             answer.push_back(quotient+1);
+//         }
+        
+//     }
+    
+    
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <functional>
 #include <iostream>
+#include <functional>
 using namespace std;
 
 vector<int> solution(int n, long long left, long long right) {
     
     vector<int> answer;
     
-    for(long long i = left; i<=right;i++){
-        long long quotient = i/n;
-        long long remainder = i%n;
+    for(long long i=left;i<=right;i++){
+        long long q = i/n; //몫
+        long long r = i%n; //나머지
         
-        if(remainder>=quotient){
-            answer.push_back(remainder+1);
+        if(q>=r){
+            answer.push_back(q+1);
         }
         else{
-            answer.push_back(quotient+1);
+            answer.push_back(r+1);
         }
-        
     }
-    
-    
+    return answer;
+}
 
+// 1 2 3 4
+// 2 2 3 4
+// 3 3 3 4
+// 4 4 4 4
     
     return answer;
 }

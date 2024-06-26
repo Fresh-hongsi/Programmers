3236b55a1cbe12b53d119c329e3c7901492a6866
Shihong Park <78722713+Fresh-hongsi@users.noreply.github.com>
2024-06-26 16:27:29 +0900

    Update H-Index.cpp

--- Lv.2/H-Index.cpp
+++ Lv.2/H-Index.cpp
@@ -1,29 +1,59 @@
+// #include <string>
+// #include <vector>
+// #include <algorithm>
+// #include <iostream>
+// #include <functional>
+// using namespace std;
+
+// int solution(vector<int> citations) {
+//     int curH = 0;
+    
+    
+//     while(1){
+//         int count = 0;
+//         for(int i=0;i<citations.size();i++){
+//             if(curH<=citations[i]){
+//                 count++;
+//             }
+//         }
+        
+//         if(count>=curH){
+//             curH++;
+//         }
+//         else{
+//             break;
+//         }
+//     }
+    
+//     return curH-1;
+// }
+
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 #include <functional>
+#include <iostream>
 using namespace std;
 
 int solution(vector<int> citations) {
-    int curH = 0;
-    
+    int answer = 0;
+    int h = 1;
     
     while(1){
         int count = 0;
         for(int i=0;i<citations.size();i++){
-            if(curH<=citations[i]){
+            if(h<=citations[i]){
                 count++;
             }
         }
-        
-        if(count>=curH){
-            curH++;
+        if(count<h){
+            answer=h-1;
+            break;
         }
         else{
-            break;
+            h++;
         }
     }
     
-    return curH-1;
+    return answer;
 }

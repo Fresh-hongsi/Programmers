59a1cedf811576e424b91a4f7f3ec87b755a9d17
Shihong Park <78722713+Fresh-hongsi@users.noreply.github.com>
2024-06-27 21:49:07 +0900

    Update 행렬의 곱셈.cpp

--- Lv.2/행렬의 곱셈.cpp
+++ Lv.2/행렬의 곱셈.cpp
@@ -1,33 +1,56 @@
+// #include <string>
+// #include <vector>
+// #include <algorithm>
+// #include <iostream>
+// #include <functional>
+// using namespace std;
+
+// vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+//     vector<vector<int>> answer;
+    
+//     answer.resize(arr1.size()); // 3x4 4x5 행렬의 크기는 3x5이므로, answer행렬의 행만 일단 세팅해둠
+    
+//     for(int i=0;i<arr1.size();i++){
+//         for(int j=0;j<arr2[0].size();j++){
+//             int temp = 0;
+//             for(int k=0;k<arr1[i].size();k++){
+//                 temp+=arr1[i][k]*arr2[k][j];
+                
+//             }
+            
+//             answer[i].push_back(temp);
+//         }
+//     }
+//     return answer;
+// }
+
+// // 3*2 2*4
+    
+// // 0
+
+// // 1 4  3 3 1 1
+// // 3 2  3 3 2 3
+// // 4 1
+
+
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 #include <functional>
 using namespace std;
 
 vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
-    vector<vector<int>> answer;
-    
-    answer.resize(arr1.size()); // 3x4 4x5 행렬의 크기는 3x5이므로, answer행렬의 행만 일단 세팅해둠
-    
+    vector<vector<int>> answer(arr1.size(),vector<int>(arr2[0].size(),0));
     for(int i=0;i<arr1.size();i++){
         for(int j=0;j<arr2[0].size();j++){
-            int temp = 0;
-            for(int k=0;k<arr1[i].size();k++){
-                temp+=arr1[i][k]*arr2[k][j];
-                
+            for(int k=0;k<arr2.size();k++){
+                answer[i][j]+=(arr2[k][j]*arr1[i][k]);
             }
-            
-            answer[i].push_back(temp);
         }
     }
     return answer;
 }
 
-// 3*2 2*4
-    
-// 0
-
-// 1 4  3 3 1 1
-// 3 2  3 3 2 3
-// 4 1
+// 1 2 3 1 1
+// 3 4 4 2 2
+//       4 4

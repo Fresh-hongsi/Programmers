e4e9c361d675eb568c477683738df0a8ad8035c4
Shihong Park <78722713+Fresh-hongsi@users.noreply.github.com>
2024-07-04 22:20:33 +0900

    Create 서울에서 김서방 찾기.java

---
+++ Java/LV1/서울에서 김서방 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String[] seoul) {
+        String answer = "";
+        int k = 0;
+        for(int i=0;i<seoul.length;i++){
+            if(seoul[i].equals("Kim")){
+                k=i;
+            }
+        }
+        
+        answer="김서방은 ";
+        answer+=Integer.toString(k);
+        answer+="에 있다";
+        return answer;
+    }
+}

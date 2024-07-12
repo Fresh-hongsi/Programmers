8aa103e5fbb686037341858d2099c99a0d67c5db
Shihong Park <78722713+Fresh-hongsi@users.noreply.github.com>
2024-07-12 20:26:29 +0900

    Create 문자열 내 마음대로 정렬하기.java

---
+++ Java/LV1/문자열 내 마음대로 정렬하기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        
+        Arrays.sort(strings,new Comparator<String>(){
+            
+            @Override
+            public int compare(String a, String b){
+                if(a.charAt(n)<b.charAt(n)){
+                    return -1;
+                }
+                else if(a.charAt(n)==b.charAt(n)){
+                    return a.compareTo(b);
+                }
+                else if(a.charAt(n)>b.charAt(n)){
+                    return 1;
+                }
+                else 
+                    return 0;
+            }
+        });
+        
+        return strings;
+    }
+}

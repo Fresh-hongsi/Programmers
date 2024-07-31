5c4f8d599da0646dfad67504cd4c5fd036f7073d
Shihong Park <78722713+Fresh-hongsi@users.noreply.github.com>
2024-07-31 19:36:35 +0900

    Update 정수 삼각형.cpp

--- Lv.3/정수 삼각형.cpp
+++ Lv.3/정수 삼각형.cpp
@@ -43,3 +43,63 @@ int solution(vector<vector<int>> triangle) {
     return answer;
 }
 
+
+
+// #include <string>
+// #include <vector>
+// #include <iostream>
+// #include <algorithm>
+// #include <functional>
+// using namespace std;
+
+// int solution(vector<vector<int>> triangle) {
+//     int answer = 0;
+    
+//     vector<vector<int>> dp(triangle.size(),vector<int>(triangle.size(),0));
+//     vector<int> result; // 마지막 행에 도달했을 때 모든 결과값을 담을 벡터
+    
+//     dp[0][0] = triangle[0][0];
+//     dp[1][0] = dp[0][0]+triangle[1][0];
+//     dp[1][1] = dp[0][0]+triangle[1][1];
+    
+//     for(int i=2;i<triangle.size();i++){
+//         for(int j=0;j<triangle[i].size();j++){
+//             if(j==0){
+//                 dp[i][j]=dp[i-1][j]+triangle[i][j];
+//             }
+//             else if(j==triangle[i].size()-1){
+//                 dp[i][j]=dp[i-1][j-1]+triangle[i][j];
+//             }
+//             else{
+//                 dp[i][j] = max(dp[i-1][j-1],dp[i-1][j])+triangle[i][j];
+//             }
+            
+//             if(i==triangle.size()-1){
+//                 result.push_back(dp[i][j]);
+//             }
+//         }
+//     }
+    
+//     answer = *max_element(result.begin(),result.end());
+//     return answer;
+// }
+
+// // 7 0 0 0 0
+// // 3 8 0 0 0 
+// // 8 1 0 0 0
+// // 2 7 4 4 0
+// // 4 5 2 6 5
+    
+// // dp[0][0]=triangle[0][0]=7
+// // dp[1][0] = dp[0][0]+triangle[1][0]=10
+// // dp[1][1] = dp[0][0]+triangle[1][1]=15
+// // dp[2][0] = dp[1][0]+triangle[2][0]=18
+// // dp[2][1] = max(dp[1][0],dp[1][1])+triangle[2][1] = 16
+// // dp[2][2] = dp[1][1]+triangle[2][2] = 15
+// // dp[3][0] = dp[2][0]+triangle[3][0]
+
+// // 7 0 0 0 0
+// // 10 15 
+// // 18 16 15
+// // 20 25 20 19
+// // 24 30 37 26 24

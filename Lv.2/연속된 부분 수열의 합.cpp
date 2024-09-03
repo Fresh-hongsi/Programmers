24c794c2311805730543cefbdf984f63a33f131a
Shihong Park <78722713+Fresh-hongsi@users.noreply.github.com>
2024-09-03 15:01:13 +0900

    Create 연속된 부분 수열의 합.cpp

---
+++ Lv.2/연속된 부분 수열의 합.cpp
@@ -0,0 +1,47 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <functional>
+#include <limits.h>
+using namespace std;
+
+// 투포인터 활용
+vector<int> solution(vector<int> sequence, int k) {
+    vector<int> answer;
+    
+    int start = 0;
+    int end = 0;
+    int sum = sequence[0];
+    int min = 99999999;
+    int resStart = 0;
+    int resEnd = 0;
+    
+    
+    while(start<sequence.size() && end<sequence.size()){
+        if(sum==k){
+            if(end-start<min){
+                resStart = start;
+                resEnd = end;
+                min = end-start;
+            }
+            end++;
+            sum+=sequence[end];
+        }
+        
+        else if(sum<k){   
+            end++;
+            sum+=sequence[end];
+        }
+        
+        else{
+            sum-=sequence[start];
+            start++;
+            
+        }
+    }
+    
+    answer.push_back(resStart);
+    answer.push_back(resEnd);
+    return answer;
+}
